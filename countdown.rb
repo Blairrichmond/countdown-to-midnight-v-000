40188d72e60775e167825ad2fb22896994cd8018
Brent  Ransom <sadumedusa@gmail.com>
2018-01-08 23:15:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,8 +7,8 @@ def countdown(int)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(x)
-   while x > 0
+def countdown_with_sleep(int)
+   while int > 0
   puts "#{x} SECOND(S)!"
    sleep 1
    x -= 1
